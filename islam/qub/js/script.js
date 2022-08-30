afb97d9ef505539047918ab513e27e466171f4b3
SidimaD <43736678+Sidimadtv@users.noreply.github.com>
2022-08-30 02:13:38 +0100

    Add files via upload

---
+++ islam/qub/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('.collapsible').collapsible();
+    $('.dropdown-trigger').dropdown();
+    $('.tooltipped').tooltip();
+});
